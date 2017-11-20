f64941220432b408f2a537aed3874b79f83a9516
Михаил Павленко <tarke7773@gmail.com>
2017-11-20 15:39:43 +0300

    anomaly detector ready

--- anomaly_detector.py
+++ anomaly_detector.py
@@ -16,15 +16,41 @@ engine = create_engine(URL(
 cnx = engine.connect()
 
 def action():
+    intervals_in_day = 24*4
+    
     meta_from_sql = pandas.read_sql('select * from test_meta', con=cnx)
-    print(meta_from_sql.describe())
+
     pairs = meta_from_sql.groupby(['api_name', 'http_method']).size().reset_index(name='counts')
     count_of_pairs = len(pairs)
-    print(count_of_pairs)
-    print(pairs)
-    pairs_in_sql = meta_from_sql[(meta_from_sql['api_name'] == pairs['api_name'][1]) &
-        (meta_from_sql['http_method'] == pairs['http_method'][1])]
-    print(pairs_in_sql.describe())
+    pair_nums = [i for i in range(count_of_pairs)]
+    for num in pair_nums:
+        print(pairs['api_name'][num]+" : "+pairs['http_method'][num])
+
+        pair_in_sql = meta_from_sql[(meta_from_sql['api_name'] == pairs['api_name'][num]) &
+            (meta_from_sql['http_method'] == pairs['http_method'][num])]
+
+        count_of_zero_intervals = intervals_in_day - pair_in_sql.count_http_code_5xx.count()
+
+        list_of_zero = [0]*count_of_zero_intervals
+        zero_intervals = pandas.DataFrame.from_items([('count_http_code_5xx', list_of_zero)])
+
+        cont_for_each_interval = pair_in_sql.count_http_code_5xx.append(zero_intervals.count_http_code_5xx, ignore_index=True)
+    
+        code_name_mean = cont_for_each_interval.mean()
+        code_name_std = cont_for_each_interval.std()
+    
+        third_sigma = 3*code_name_std
+    
+        mean_plus_th_sigma = code_name_mean + third_sigma
+        
+        print(mean_plus_th_sigma)
+        print('\n')
+    
+        meta_from_sql.loc[(meta_from_sql['api_name'] == pairs['api_name'][num]) &
+            (meta_from_sql['http_method'] == pairs['http_method'][num]) &
+            (meta_from_sql['count_http_code_5xx'] > mean_plus_th_sigma), 'is_anomaly'] = 1
+    meta_from_sql.to_sql(name='test_meta', con=cnx, if_exists = 'replace', index=False)
+    
     
 if __name__ == '__main__':
     action()
